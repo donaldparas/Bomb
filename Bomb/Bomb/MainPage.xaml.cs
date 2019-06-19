65e3865ccb0ee641f4245ddd3ad707cf22b77ce1
Donald P. Paras <donaldpp@ad-fai.com>
2019-06-19 10:07:12 +0800

    2nd commit

---
+++ Bomb/Bomb/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Bomb
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        static string bomb = new Random().Next(1, 4).ToString();
+        static int Scores = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void Pindutin(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (button.Text == bomb)
+            {
+                await DisplayAlert("Yari Ka", "Sabog", "Subuki Ulet");
+                bomb = new Random().Next(1, 4).ToString();
+                Scores = 0;
+            }
+            else
+            {
+                Scores += 1;
+                await DisplayAlert("Galing", "Score:", +Scores + "Subuki Ulet");
+
+            }
+        }
+
+    }
+}
