d40706842fb14213681efb0714388403b855dbd2
Donald P. Paras <donaldpp@ad-fai.com>
2019-06-19 13:44:48 +0800

    4th Commit working code

--- Bomb/Bomb/MainPage.xaml.cs
+++ Bomb/Bomb/MainPage.xaml.cs
@@ -1,41 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace Bomb
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
-    [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
-    {
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : ContentPage
+	{
         static string bomb = new Random().Next(1, 4).ToString();
         static int Scores = 0;
-        public MainPage()
-        {
-            InitializeComponent();
-        }
+        public MainPage ()
+		{
+			InitializeComponent ();
+		}
+
         async void Pindutin(object sender, EventArgs e)
         {
             Button button = sender as Button;
 
             if (button.Text == bomb)
             {
-                await DisplayAlert("Yari Ka", "Sabog", "Subuki Ulet");
+                await DisplayAlert("Yari Ka", "Sabog", "Subukan mo Ulit");
                 bomb = new Random().Next(1, 4).ToString();
                 Scores = 0;
             }
             else
             {
                 Scores += 1;
-                await DisplayAlert("Galing", "Score:", +Scores + "Subuki Ulet");
+                await DisplayAlert("Galing", "Score:", +Scores + "Subukan mo Ulit");
 
             }
         }
-
     }
 }
